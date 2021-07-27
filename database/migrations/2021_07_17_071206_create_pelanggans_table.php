1e6bbf51ac6f443057f32340ba5ef3908c9485ef
portgazdbeel <portgaz77@gmail.com>
2021-07-27 20:21:56 +0700

    first commit

---
+++ database/migrations/2021_07_17_071206_create_pelanggans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePelanggansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pelanggans', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_user')->unsigned()->nullable(true);
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->string('nama');
+            $table->text('alamat');
+            $table->string('no_hp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pelanggans');
+    }
+}
